f1d4c5ca7129fe5a15d6ef058d0dd657ba40c61b
dreamonus <dmarkin@ua.fm>
2015-02-21 22:21:06 +0200

    Initial commit

---
+++ js/slider.js
@@ -0,0 +1,77 @@
+'use strict';
+
+(function () {
+    var images = ['images/image01.jpg',
+        'images/image02.jpg',
+        'images/image03.jpg',
+        'images/image04.jpg'];
+
+    function Slider(node, arrayOfImages) {
+        this.node = node;
+        this.images = images;
+        this.createSliderStructure(this.node);
+        this.currentSlide = 0;
+        this.sliderTimer = null;
+        this.slideWidth = 910;
+        this.delegateEvents();
+        this.runSlider();
+    }
+
+    //create slider DOM nodes
+    Slider.prototype.createSliderStructure = function (node) {
+        $(node).append(($('<div>').addClass('panel'))
+            .append($('<div>').addClass('shortcut gray active').text('Work and welcomes guests in a euyutny studio'))
+            .append($('<div>').addClass('shortcut rose').text('Relax in comfortable silence bathroom'))
+            .append($('<div>').addClass('shortcut red').text('Enjoy your stay in a cozy bedroom'))
+            .append($('<div>').addClass('shortcut pink').text('Spend your time in games with children in the nursery')))
+            .append($('<div>').addClass('slides-block')
+                .append($('<div>').addClass('slides')
+                    .append($('<img>').attr('src', this.images[0]))
+                    .append($('<img>').attr('src', this.images[1]))
+                    .append($('<img>').attr('src', this.images[2]))
+                    .append($('<img>').attr('src', this.images[3]))));
+    }
+
+    Slider.prototype.runSlider = function () {
+        this.sliderTimer = setInterval(this.nextSlide.bind(this), 3000);
+    }
+
+    Slider.prototype.nextSlide = function (slideNum) {
+        this.currentSlide += 1;
+        if (this.currentSlide === 4) {
+            this.currentSlide = 0;
+        }
+        this.animate(this.currentSlide);
+    }
+
+    //animated scrolling
+    Slider.prototype.animate = function (slideNum) {
+        var _this = this;
+
+        this.node.find('.slides').animate({left: -slideNum * this.slideWidth}, 400, function () {
+            _this.node.find('.shortcut.active').removeClass('active');
+            _this.node.find('.shortcut').eq(slideNum).addClass('active');
+        });
+    }
+
+    //set event listeners for slider
+    Slider.prototype.delegateEvents = function () {
+        var _this = this;
+
+        this.node.find('.shortcut').on('click', (function () {
+            clearTimeout(_this.pause);
+            _this.pause = null;
+            var goToNum = _this.node.find('.shortcut').index(this);
+            _this.currentSlide = goToNum - 1;
+            _this.nextSlide();
+            clearInterval(_this.sliderTimer);
+            _this.pause = setTimeout(function () {
+                _this.runSlider();
+            }, 3000);
+        }));
+    }
+
+    //create 2 sliders (for example)
+    window.slider1 = new Slider($('.slider-1'), images);
+    window.slider2 = new Slider($('.slider-2'), images);
+})();
---
+++ readme.md
@@ -0,0 +1,7 @@
+**This slider** is composed of a fixed number of slides (4 pieces), the images are of the same size in advance known.
+Animated transitions between slides. By clicking on the plate on the left there is a transition to the corresponding slider, compliance is determined by the order.
+The upper die - the first slide, the second from the top - the second, and so on.
+Immediately after the initialization mode is activated at the slider autoscroll: show the next slide every 3 seconds.
+If the user clicks on one of the shortcut, navigate to the slide you want to disable auto scroll. Autoscroll need to include 5 seconds after the last time a user clicks on the shortcut.
+Autoscroll cyclic: with the last slide there is a transition to first. **You can lay a few sliders.**
+Used jQuery. *Slider works in IE9+.*
